a93e8a43521099a19882704f85385acba55c5dc5
YANAGISAWA Chihiro <chihiroyn@gmail.com>
2020-04-11 07:43:23 +0900

    Succeded in running server

--- main.go
+++ main.go
@@ -1,21 +1,74 @@
 package main
 
 import (
+	"context"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 	"log"
 	"net/http"
+	"strings"
 )
 
-// TODO: Schema
+// Schema
+const Schema = `
+type Vegetable {
+    name: String!
+    price: Int!
+    image: String
+}
+type Query {
+    vegetable(name: String!): Vegetable
+}
+schema {
+    query: Query
+}
+`
+
+type Vegetable struct {
+	name  string
+	price int
+	image *string
+}
+
+var vegetables map[string]Vegetable
+
+// Utils
+func strPtr(str string) *string {
+	return &str
+}
+
+type VegetableResolver struct {
+	v *Vegetable
+}
+
+func (r *VegetableResolver) Name() string   { return r.v.name }
+func (r *VegetableResolver) Price() int32   { return int32(r.v.price) }
+func (r *VegetableResolver) Image() *string { return r.v.image }
+
 // TODO: Model
 type query struct{}
 
 // TODO: Resolver
+func (q *query) Vegetable(ctx context.Context, args struct{ Name string }) *VegetableResolver {
+	v, ok := vegetables[strings.ToLower(args.Name)]
+	if ok {
+		return &VegetableResolver{v: &v}
+	}
+	return nil
+}
+
+
 func main() {
 	schema := graphql.MustParseSchema(Schema, &query{})
 	http.Handle("/query", &relay.Handler{Schema: schema})
-	// TODO: init model
+
+	// init model
+	vegetables = map[string]Vegetable{
+		"tomato": Vegetable{name: "Tomato", price: 100, image: strPtr("https://picsum.photos/id/152/100/100")},
+		"potato": Vegetable{name: "Potato", price: 50, image: strPtr("https://picsum.photos/id/159/100/100")},
+		"corn":   Vegetable{name: "Corn", price: 200},
+	}
+
 	// TODO: graphiql
 	activateGraphiql()
 
